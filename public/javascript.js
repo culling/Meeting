f7e38a9bcb70ec886e3dc4847a26ec8a3266bcab
culling <culling66@gmail.com>
2017-04-05 12:25:37 +0800

    Working Random Cell movement

--- public/javascript.js
+++ public/javascript.js
@@ -1,20 +1,53 @@
 
+
 class TestComponent extends React.Component {
     constructor(){
         super();
-        this.state=({})    
-    }
-    
-    render(){
-        
-        return (
-    <div> 
-        <h3>React is Go!</h3> 
+        this.state = ({
+            rows: 10,
+            cellHeight: 10,
+            columns: 10,
+            cellWidth: 10,
+            viewPortCSS: {
+                "background-color":"blue"
+            }
 
-        <div className="container">
-        </div>
+        })
+    }
+    render(){
+        return (
+    <div id="body-container">
+        <h1> React is Go! </h1>
+        <svg    width={this.state.cellWidth * this.state.columns}
+                height={this.state.cellHeight * this.state.rows}  
+                style={this.state.viewPortCSS}
+                >
+                
+                <Cell   cellHeight={this.state.cellHeight} 
+                        cellWidth= {this.state.cellWidth} 
+                        x={ (this.state.columns -1)* this.state.cellHeight * (Math.random()) } 
+                        y={ (this.state.rows    -1)* this.state.cellHeight * (Math.random()) } />
+        </svg>
     </div>
         );
+    }
+}
+
+class Cell extends React.Component{
+    constructor(){
+        super();
+    
+    }
+
+    render(){
+        return(
+            <rect   x={this.props.x} 
+                    y={this.props.y} 
+                    height={this.props.cellHeight} 
+                    width={this.props.cellWidth} 
+                    stroke="white"
+                    />
+        )
     }
 
 
