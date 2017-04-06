6f365a22a2f861fc4eda3f1e4b6c005aa20a8a76
culling <culling66@gmail.com>
2017-04-06 12:07:12 +0800

    Create Random Cells and add them to the Cell Array

--- public/javascript.js
+++ public/javascript.js
@@ -9,7 +9,13 @@ class TestComponent extends React.Component {
             columns: 10,
             cellWidth: 10,
             viewPortCSS: {
-                "background-color":"blue"
+                "backgroundColor":"blue"
+            },
+            cellsArray: [],
+            blankCell:  {
+                column: 0,
+                row:    0,
+                filled: true
             }
 
         })
@@ -18,18 +24,41 @@ class TestComponent extends React.Component {
         return (
     <div id="body-container">
         <h1> React is Go! </h1>
+        <button onClick={this._makeRandomCell.bind(this) } > Make Random Cell </button>
+        <br />
         <svg    width={this.state.cellWidth * this.state.columns}
                 height={this.state.cellHeight * this.state.rows}  
                 style={this.state.viewPortCSS}
                 >
-                
-                <Cell   cellHeight={this.state.cellHeight} 
+                {this.state.cellsArray.map((cell, i) => <Cell 
+                        key={i}
+                        cellHeight={this.state.cellHeight} 
                         cellWidth= {this.state.cellWidth} 
-                        x={ (this.state.columns -1)* this.state.cellHeight * (Math.random()) } 
-                        y={ (this.state.rows    -1)* this.state.cellHeight * (Math.random()) } />
+                        column=     {cell.column}
+                        row=        {cell.row}
+                />)}
+
+                {/*<Cell   cellHeight={this.state.cellHeight} 
+                        cellWidth= {this.state.cellWidth} 
+                        column={ ( Math.floor((this.state.columns -1)*(Math.random())) ) } 
+                        row=   { ( Math.floor((this.state.rows    -1)*(Math.random())) ) }/>
+                        */}
         </svg>
     </div>
         );
+    }
+
+    _makeRandomCell(){
+        let cell = Object.assign({},this.state.blankCell);
+        console.log(this.state.cellsArray);
+        
+        let localCellsArray = this.state.cellsArray.map(element => element );
+                
+        cell.column = ( Math.floor((this.state.columns -1)*(Math.random())) );
+        cell.row    = ( Math.floor((this.state.rows    -1)*(Math.random())) );
+        cell.filled = true;
+        localCellsArray.push(cell);
+        this.setState({cellsArray: localCellsArray})
     }
 }
 
@@ -41,17 +70,17 @@ class Cell extends React.Component{
 
     render(){
         return(
-            <rect   x={this.props.x} 
-                    y={this.props.y} 
+            <rect   x={this.props.column * this.props.cellWidth} 
+                    y={this.props.row    * this.props.cellHeight} 
                     height={this.props.cellHeight} 
                     width={this.props.cellWidth} 
                     stroke="white"
                     />
         )
     }
-
-
 }
+
+
 
 ReactDOM.render (
     <TestComponent />, document.getElementById('mount-point')
