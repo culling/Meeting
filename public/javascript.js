ea4b8822c7091b0ffe7f8354363356183d8109d0
culling <culling66@gmail.com>
2017-04-09 10:58:21 +0800

    drawing walls

--- public/javascript.js
+++ public/javascript.js
@@ -11,6 +11,10 @@ class GameOfLifeComponent extends React.Component {
             viewPortCSS: {
                 "backgroundColor":"blue"
             },
+            roomSettings:{
+                minWidth:   3,
+                minHeight:  3
+            },
 
             tiles:{
                 wall:{
@@ -36,8 +40,12 @@ class GameOfLifeComponent extends React.Component {
         //this.randomtiles.focus();
     }
 
-    componentDidMount(){
+    componentWillMount(){
         this._generateMap();
+    }
+
+    componentDidMount(){
+        this._generateRandomPath();
     }
 
     render(){
@@ -54,12 +62,7 @@ class GameOfLifeComponent extends React.Component {
 
                             </div>
                             <div className="col-md-6">
-                                {/* this.state.gameBoard.map((tile ,i) => 
-                                    <div key = {i}>
-                                        <p> Column: {tile.column} </p>
-                                        <p> Row: {tile.row}</p>
-                                        <p> {i} </p>
-                                    </div>  ) */}
+
                             <svg    width={this.state.tileWidth * this.state.columns}
                                     height={this.state.tileHeight * this.state.rows}  
                                     style={this.state.viewPortCSS}
@@ -90,19 +93,14 @@ class GameOfLifeComponent extends React.Component {
             for(let y=0; y < this.state.rows; y++){
 
                 let currentTile = Object.assign({},  this.state.tiles.floor);
-
+                
                 if((x==0 ) || (x== this.state.columns -1) ||
                     (y== 0) || (y == this.state.rows -1)){
                         currentTile = Object.assign({}, this.state.tiles.wall);
                 }
-
             
                 currentTile.column = x;
                 currentTile.row    = y;
-
-                if (x == 1){
-                    console.log(currentTile);
-                }
 
                 mapArray.push( currentTile );                
             }
@@ -111,14 +109,47 @@ class GameOfLifeComponent extends React.Component {
         this.setState({gameBoard: mapArray});
     }
 
+    _generateRandomPath(){
+        let currentGameBoard =  this.state.gameBoard.map(tile => tile);
+        let minColumn = 1;
+        let maxColumn = this.state.columns - 2;
+        let rangeColumn = maxColumn - minColumn;
+
+        let minRow = 1;
+        let maxRow = this.state.rows -2; 
+        let rangeRow = maxRow - minRow;
+        
+        let path = [];
+
+        let length = 4;
+
+        let randomRow    = minRow    + (Math.floor (rangeRow    * Math.random()) );
+        let randomColumn = minColumn + (Math.floor (rangeColumn * Math.random()) );
+
+        let row    = 3;
+        for (let i = 0; i < length; i++){
+            let wallTile = Object.assign({}, this.state.tiles.wall); 
+            wallTile.row    = randomRow;
+            wallTile.column = randomColumn + i ;
+            currentGameBoard[ wallTile.row  + ((this.state.columns) * wallTile.column) ] = wallTile;
+            console.log(wallTile);
+        }
+
+        //[(row * this.state.columns) + column ]
+        //console.log(path)
+        this.setState({gameBoard: currentGameBoard});
+    }
+
+
     _showState(){
         console.log(this.state)
     }
 
     _getTile(column, row){
-
         return this.state.gameBoard[(row* this.state.columns) + column ]
     }
+
+
 
 }
 
