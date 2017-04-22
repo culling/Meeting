7ff32e7c4764ca39e9f5664626a681be90cacc21
culling <culling66@gmail.com>
2017-04-22 21:34:37 +0800

    ready for submission as heatmap

--- public/javascript.js
+++ public/javascript.js
@@ -17,59 +17,18 @@ function getJSON() {
     $.getJSON(url, function(json) {
 
         let h = window.innerHeight * 0.6;
-        let w = window.innerWidth  * 0.6;
-        let barPaddingWidth = 1;
-        let yPadding = 30;
-        let xPadding = 80;
-            /*
-            .on("mouseover", function(d){
-                d3.select(this)
-                    .attr("r", circleSize * 3);
-                var xPosition = parseFloat(d3.event.pageX)
-                var yPosition = parseFloat(d3.event.pageY +14)
+        let w = window.innerWidth  * 0.9;
 
-                d3.select("#tooltip")
-                  .style("left", xPosition + "px")
-                  .style("top", yPosition + "px");
-
-                d3.select("#tooltip")
-                  .select("#Name")
-                  .text(d.Name);
-
-                d3.select("#tooltip")
-                    .select("#Doping")
-                    .append("div")
-                    .attr("class", "dopeStoryLink")
-                    .html('<a href="'+d.URL +'" >'+ d.Doping+"</a>" )
-
-                d3.select("#tooltip").classed("hidden", false);
-                
-            })
-            .on("mouseout", function(){
-                d3.select(this)
-                    .attr("r", circleSize);
-                d3.select("#tooltip")
-                    .transition()
-                    .delay(5000)
-                    .attr("class", "hidden");
-            })
+        let getMonth =      d3.time.format("%m");
+        let getMonthName =  d3.time.format("%B");
+        let yearFormat = d3.time.format("%Y");
 
 
-        d3.select("#tooltip")
-                .on("mouseover", function(d){
-                d3.select(this).classed("hidden", false)
-            })
-                .on("mouseout", function (d){
-                d3.select(this).classed("hidden", true)            
-            });
-            */
-
-        
-
-        let getMonth = d3.time.format("%m");
-        let getMonthName = d3.time.format("%B");
-        console.log(json);
-        console.log(json.monthlyVariance[0].month);
+        //console.log(json);
+        //console.log(json.monthlyVariance[0].month);
+        let years = Math.ceil(json.monthlyVariance.length /12);
+        //console.log(years);
+        let rectHeight = ((h /12)*1.1 );
 
         let months = [  "January",
                         "February",
@@ -83,14 +42,34 @@ function getJSON() {
                         "October",
                         "November",
                         "December"]
-        let margins = {top: 20,
-                        left: 20,
-                        bottom: 20,
-                        right: 20}
+
+        let margins = {top:     h*0.05,
+                        left:   w*0.05,
+                        bottom: h*0.10,
+                        right:  w*0.05}
+
+        let legendElementWidth = margins.left * 4;
+
+
+        let xScale = d3.time.scale()
+            .domain([   d3.min(json.monthlyVariance, (data) => { return (yearFormat.parse(data.year.toString() )) }),
+                        d3.max(json.monthlyVariance, (data) => { return (yearFormat.parse(data.year.toString() )) })
+            ])
+            .range([0, (w - ( legendElementWidth  )) ]);
+
+        
+        let xAxis = d3.svg.axis();
+            xAxis.scale(xScale);
+            xAxis.ticks(d3.time.year, 20);
+            xAxis.orient("bottom")
+            xAxis.tickFormat(d3.time.format("%Y"));
+
+
+
 
         let svg = d3.select("#graph")
                     .append("svg")
-                    .attr("width",  w + margins.left + margins.right )
+                    .attr("width",  w + margins.left +( margins.right * 2) )
                     .attr("height", h + margins.top  + margins.bottom)
                     .attr("style", 'background-color: teal');
 
@@ -98,23 +77,104 @@ function getJSON() {
                 .data(months)
                 .enter()
                 .append("text")
-                .text( (month)=>{return month} )
+                .text( (month)          =>{return month} )
                 .attr("x", margins.left)
-                .attr("y", (month, i )=>{return (i * (h/months.length)) + margins.top } )
-                .attr("class", ".monthLabel");
+                .attr("y", (month, i )  =>{return ( (i%12) * (h/months.length)) + (margins.top*2) })
+                .attr("class", "monthLabel");
 
 
-            /*.selectAll("p")
-            .data(json.monthlyVariance)
-            .enter()
-            */
-            /*.append("p")
-            .text( (line) =>{
-                let d3Month   = getMonth.parse( line.month.toString() );
-                return '"'.concat( getMonthName( (d3Month))).concat('",') ;
+            let blocks = svg.selectAll(".year")
+                .data(json.monthlyVariance);
+
+
+            blocks.enter()
+                .append("rect")
+                .attr("x", (line,  i )=>{return legendElementWidth + (Math.floor(i/12) * ( ( (w - legendElementWidth)*(1/years) /*-(legendElementWidth + (margins.right *15) ) ) / (12*12*/)  ) ) })
+                .attr("y", (month, i )=>{return ( (i%12) * (h/months.length)) + margins.top })
+                .attr("class", "year")
+                .attr("rx", 4)
+                .attr("ry", 4)
+                .attr("width", (w - legendElementWidth)*(1/years))
+                .attr("height", rectHeight )
+                .attr("style", (line)=>{
+                    let red   = 120 - Math.floor(line.variance * 50);
+                    let green = 0;
+                    let blue  = 120 + Math.floor(line.variance * 50);
+                    let colorString = red.toString().concat(", ")
+                                         .concat(green)
+                                         .concat(", ")
+                                         .concat(blue);
+                        //console.log(colorString);
+                    let color = 'fill: rgb('.concat(colorString).concat(')');
+                    //let color = "rgb(200,155,120)";
+                    //console.log(color);
+                    return (color) })
+            
+            .on("mouseover", function(d){
+                d3.select(this)
+                    .attr("h", rectHeight * 3);
+                let xPosition = parseFloat(d3.event.pageX)
+                let yPosition = parseFloat(d3.event.pageY +14)
+
+                let d3Month   = getMonth.parse( d.month.toString() );
+                let d3MonthName =  ( getMonthName( (d3Month))) ;
+        
+                let absoluteTemp = json.baseTemperature + d.variance;
+
+
+                let formatTemp = d3.format(".3n");
+
+                d3.select("#tooltip")
+                  .style("left", xPosition + "px")
+                  .style("top", yPosition + "px");
+
+                d3.select("#tooltip")
+                  .select("#Year")
+                  .text(d.year);
+
+                d3.select("#tooltip")
+                  .select("#Month")
+                  .text( d3MonthName );
+
+                d3.select("#tooltip")
+                    .select("#Temprature")
+                    .text( formatTemp(absoluteTemp));
+
+                d3.select("#tooltip")
+                    .select("#variance")
+                    .text(d.variance);
+
+
+                d3.select("#tooltip").classed("hidden", false);
+                
+            })
+            /*
+            .on("mouseout", function(){
+                d3.select(this)
+                    .attr("r", circleSize);
+                d3.select("#tooltip")
+                    .transition()
+                    .delay(5000)
+                    .attr("class", "hidden");
             })
             */
 
+            
+        d3.select("#tooltip")
+                .on("mouseover", function(d){
+                d3.select(this).classed("hidden", false)
+            })
+                .on("mouseout", function (d){
+                d3.select(this).classed("hidden", true)            
+            });
+            
+
+        
+
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + legendElementWidth + ", " + (h + (margins.bottom/2) ) + ")")
+            .call(xAxis);
 
     });
 }
