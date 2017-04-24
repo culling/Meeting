1f6b30d3d9e89e53efe21da4bf60a16642e23e69
culling <culling66@gmail.com>
2017-04-24 13:32:46 +0800

    Submitted to codepen - had to work out about layering the images

--- public/javascript.js
+++ public/javascript.js
@@ -1,8 +1,6 @@
 // Visualize with force directed graph
 // 
 // Page 167 - Learning d3dotjs - Data Visualisation Second Generation
-// https://bl.ocks.org/mbostock/3750558
-// https://codepen.io/leoCecilia/pen/dvvmZj/
 //
 
 
@@ -14,8 +12,6 @@ $('document').ready(function() {
 
 function getJSON() {
 
-    //let url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
-    //let url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json"
     let url = "https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json";
 
     $.getJSON(url, function(json) {
@@ -29,8 +25,6 @@ function getJSON() {
                         bottom: h*0.10,
                         right:  w*0.05}
 
-        //let legendElementWidth = margins.left * 4;
-
         
         let svg = d3.select("#graph")
                     .append("svg")
@@ -38,43 +32,35 @@ function getJSON() {
                     .attr("height", h + margins.top  + margins.bottom)
                     .attr("style", 'background-color: teal');
             
-            /*
-            svg.append("defs")
-                .html(`
-                    <pattern id="image" x="0" y="0" patternUnits="userSpaceOnUse" height="120" width="120">
-                        <image x="0" y="0" xlink:href="https://www.cs.mun.ca/~h65ped/Public/country%20data%20for%20force%20directed%20graph/flags.png"></image>
-                    </pattern>
-                    `);
-            */
+
 
         let link = svg.selectAll(".link");
-        let node = svg.selectAll(".node");
+        let node =  d3.select('#flags').selectAll(".node");
 
-
+        
         let nodes = json.nodes;
         let links = json.links;
 
-
-        let linkSize = 5;
+        let linkSize = 8 *(h / nodes.length );
 
         let force = d3.layout.force()
                     .nodes( nodes )
                     .links( links )
-                    .charge( (linkSize * 5) * -1 )
+                    .charge( (linkSize  ) * -1 )
                     .gravity(0.05)
-                    .size([w, h])
+                    .size([w, h + 20 ])
                     .start();
 
-            force.on("tick", tick);
-
-        
-        
         link = link.data( links )
             .enter()
             .append("line")
             .attr("class", "link")
-            .attr("style", "stroke-width: 2; stroke:black;")
-        
+            .attr("style", "stroke-width: 2; stroke:black;");
+
+
+
+
+
         node = node.data(nodes)
             .enter()
             .append("img")
@@ -82,66 +68,45 @@ function getJSON() {
                 let flagCode = "flag flag-".concat(line.code);
                 return "node ".concat(flagCode);
             })
-            .attr("width", 20)
-            .attr("height", 20);
+            .on("mouseover", function(d){
+                d3.select(this)
+                    
+                let xPosition = parseFloat(d3.event.pageX)
+                let yPosition = parseFloat(d3.event.pageY +14)
 
-            //.html( '<p style="position: absolute;   display: inline-block; ">Test</p>' )
-            /*
-            .append("circle")
-            //.append("div")
-            .attr("class", (line,i ) => {
-                let flagCode = "flag flag-".concat(line.code);
-                return "node ".concat(flagCode);
-            })*/
+                d3.select("#tooltip")
+                  .style("left", xPosition + "px")
+                  .style("top", yPosition + "px");
 
-            /*
-            .attr("style", (line)=>{
-                let red   =  Math.floor(Math.random() * 250);
-                let green =  0;
-                let blue  =  Math.floor(Math.random() * 250);
-                let colorString = red.toString().concat(", ")
-                                        .concat(green)
-                                        .concat(", ")
-                                        .concat(blue);
-                    //console.log(colorString);
-                let color = 'fill: rgb('.concat(colorString).concat(')');
-                //return (color) 
-                
-                let imgUrl = "https://www.cs.mun.ca/~h65ped/Public/country%20data%20for%20force%20directed%20graph/flags.png";
-                //let fill = 'fill: url("' + imgUrl +'")';
+                d3.select("#tooltip")
+                    .select("#Country")
+                    .html(function(){
+                        let wikiCountryName  = d.country.replace(/ /g,"_");
+                        let wikiPage = `https://en.wikipedia.org/wiki/${wikiCountryName}`;
+                        let html     = `<a href=${wikiPage}>${d.country}</a>`
+                        return html;
+                    })
 
-                //background-position: -16px 0
-                let fill = 'fill: url("#image")';
-                return fill;
-            })
-            .attr("r", linkSize);
-            */
-
+                d3.select("#tooltip").classed("hidden", false); 
+            });
+  
+        force.on("tick", tick);
 
         function tick() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
 
 
-        node.attr("style", function(d){  
-                let ws = "left: ".concat( Math.round( d.x)).concat("; top: ").concat( Math.round(d.y)) ;
-                    ws = ws.concat(';background: url("https://www.cs.mun.ca/~h65ped/Public/country%20data%20for%20force%20directed%20graph/flags.png") no-repeat')
-                    ws = ws.concat(';background-position: -16px 0;')        
-
-                    //background:url("https://www.cs.mun.ca/~h65ped/Public/country%20data%20for%20force%20directed%20graph/flags.png") no-repeat
-                return ws;
-        });
-
+            node.style('left',function(d){
+                return (d.x -5 )+'px';
+            }).style('top',function(d){
+                return (d.y + 130 )+'px';
+            });
         };
 
-        /*
-        node.style('left', d => (d.x - 8) + "px")
-			.style('top', d => (d.y - 5) + "px");
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
-        }*/
+
 
 
         
